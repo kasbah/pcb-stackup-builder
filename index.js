f9ae22e5c20273294547cef41a6d9409468b22e3
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-09 13:16:02 +0100

    WIP start writing wrapper functions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var async = require('async')
 var shortId = require('shortid')
 var gerberToSvg = require('gerber-to-svg')
 var whatsThatGerber = require('whats-that-gerber')
-var pcbStackup = require('pcb-stackup')
+var stackupProcess = require('pcb-stackup')
 
 var gerberPaths = [
   'gerbers/BusPirate-v3.6-SSOP.cmp',
@@ -17,7 +17,29 @@ var gerberPaths = [
   'gerbers/BusPirate-v3.6-SSOP.sts'
 ]
 
-// asynchronously map a gerber filename to a layer object expected by pcbStackup
+var processLayer = function (layer) {
+  if (layer.layerType == null) {
+    layer.layerType = whatsThatGerber(layer.filename).id
+  }
+  layer.gerber = gerberToSvg(gerber, converterOptions, function(error, result) {
+    if (error) {
+      console.warn(' failed to convert')
+    }
+  });
+}
+
+var pcbStackup = function (layers, optionsOrCallback, callback) {
+  var options;
+  if (typeof optionsOrCallback === 'object') {
+    options = optionsOrCallback;
+  } else if (typeof optionsOrCallback === 'function') {
+    callback = optionsOrCallback;
+  }
+  console.log('options', options);
+  console.log('callback', callback);
+}
+
+// asynchronously map a gerber filename to a layer object expected by stackupProcess
 var mapFilenameToLayerObject = function(filename, done) {
   var gerber = fs.createReadStream(filename, 'utf-8')
   var type = whatsThatGerber(filename)
@@ -36,16 +58,16 @@ var mapFilenameToLayerObject = function(filename, done) {
   })
 }
 
-// pass an array of layer objects to pcbStackup and write the stackup results
+// pass an array of layer objects to stackupProcess and write the stackup results
 var handleLayers = function(error, layers) {
   if (error) {
     return console.error('error mapping gerber file paths to array of converters')
   }
 
-  var stackup = pcbStackup(layers.filter(Boolean), {id: 'my-board', maskWithOutline: true})
+  var stackup = stackupProcess(layers.filter(Boolean), {id: 'my-board', maskWithOutline: true})
   fs.writeFileSync('top.svg', stackup.top)
   fs.writeFileSync('bottom.svg', stackup.bottom)
 }
 
-// map the gerber files to layer objects, then pass them to pcbStackup
+// map the gerber files to layer objects, then pass them to stackupProcess
 async.map(gerberPaths, mapFilenameToLayerObject, handleLayers)
